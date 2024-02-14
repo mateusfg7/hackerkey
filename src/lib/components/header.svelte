e5520c62ca2a59b1cdedbe36fc8d59641f22f030
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-02-14 19:49:05 -0300

    feat(header): create Header component

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,8 @@
+<script>
+	import ThemeToggle from './theme-toggle.svelte';
+</script>
+
+<header class="flex items-center justify-between bg-primary p-2 text-primary-foreground">
+	<h1 class="text-2xl font-bold">Hacker Key</h1>
+	<ThemeToggle class="bg-primary-foreground text-primary" />
+</header>
